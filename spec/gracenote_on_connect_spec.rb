b1a413a808d380ddf06e022f7e0c0c59309df229
Matt Morris <matt.morris@placewise.com>
2015-06-10 15:10:48 -0600

    add spec section for showtimes

--- spec/gracenote_on_connect_spec.rb
+++ spec/gracenote_on_connect_spec.rb
@@ -8,11 +8,21 @@ describe Gracenote::On::Connect do
   describe 'Theater' do
     describe 'finds by zip' do
       before do
-        @theaters = Gracenote::On::Connect::Theater.find_by_zip(78701)
+        @theaters = Gracenote::On::Connect::Theater.find_by_zip(44113)
       end
 
       it 'has results' do
         expect(@theaters).to_not be_empty
+      end
+    end
+
+    describe 'finds showtimes' do
+      before do
+        @showtimes = Gracenote::On::Connect::Theater.showtimes(2279)
+      end
+
+      it 'has results' do
+        expect(@showtimes).to_not be_empty
       end
     end
   end
