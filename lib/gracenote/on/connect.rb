d29a8068779699cf308099b2e0fae7544e35d3b0
Matt Morris <matt.morris@placewise.com>
2015-06-10 15:10:28 -0600

    extract base_url

--- lib/gracenote/on/connect.rb
+++ lib/gracenote/on/connect.rb
@@ -7,11 +7,12 @@ module Gracenote
   module On
     module Connect
       API_KEY = ENV['API_KEY']
+      BASE_URL = 'http://data.tmsapi.com/v1.1'
 
       class Theater
         def self.find_by_zip(zip)
           params = { api_key: API_KEY, zip: zip }
-          uri = URI('http://data.tmsapi.com/v1.1/theatres')
+          uri = URI("#{BASE_URL}/theatres")
           uri.query = URI.encode_www_form(params)
 
           results = Net::HTTP.get(uri)
@@ -21,7 +22,7 @@ module Gracenote
 
         def self.showtimes(theater_id, start_date = Date.today.to_s, days = 1)
           params = { api_key: API_KEY, startDate: start_date, numDays: days }
-          uri = URI("http://data.tmsapi.com/v1.1/theatres/#{theater_id}/showings")
+          uri = URI("#{BASE_URL}/theatres/#{theater_id}/showings")
           uri.query = URI.encode_www_form(params)
 
           results = Net::HTTP.get(uri)
