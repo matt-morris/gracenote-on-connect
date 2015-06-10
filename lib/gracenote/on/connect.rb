f11330b24f88573260001cf1bda68cfbd1bab447
Matt Morris <matt.morris@placewise.com>
2015-06-10 15:08:46 -0600

    add showtimes method

--- lib/gracenote/on/connect.rb
+++ lib/gracenote/on/connect.rb
@@ -1,6 +1,7 @@
 require 'gracenote/on/connect/version'
 require 'net/http'
 require 'json'
+require 'date'
 
 module Gracenote
   module On
@@ -14,6 +15,17 @@ module Gracenote
           uri.query = URI.encode_www_form(params)
 
           results = Net::HTTP.get(uri)
+          # binding.pry
+          JSON.parse(results)
+        end
+
+        def self.showtimes(theater_id, start_date = Date.today.to_s, days = 1)
+          params = { api_key: API_KEY, startDate: start_date, numDays: days }
+          uri = URI("http://data.tmsapi.com/v1.1/theatres/#{theater_id}/showings")
+          uri.query = URI.encode_www_form(params)
+
+          results = Net::HTTP.get(uri)
+          # binding.pry
           JSON.parse(results)
         end
       end
